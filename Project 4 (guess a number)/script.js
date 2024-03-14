0953c392c191b41f4d6a01a81442b8273efd644c
karanMalhii <karanmalhii1313@gmail.com>
2024-03-14 09:57:17 +0530

    guess a number pending 10:00

---
+++ Project 4 (guess a number)/script.js
@@ -0,0 +1,28 @@
+console.log(parseInt(Math.random() * 100 + 1));
+
+const submit = document.querySelector("#submitMe");
+const userInput = document.querySelector("#guessField");
+const guessSlot = document.querySelector(".guesses");
+const remaining = document.querySelector(".lastResult");
+const lowOrHi = document.querySelector(".lowOrHi");
+const resultParas = document.querySelector(".resultParas");
+
+const p = document.createElement("p");
+
+let prevGuess = [];
+let numGuess = 1;
+
+let playGame = true;
+
+if (playGame) {
+  submit.addEventListener("click", function (e) {
+    e.preventDefault();
+    const guess = parseInt(userInput.value);
+    console.log(guess);
+    validateGuess(guess);
+  });
+}
+
+function validateGuess(guess) {
+
+}
